535153dc8d96fcea7da76d1db367c75e853acca5
saisirichandra <sirichandrapodugu@gmail.com>
2021-05-12 15:05:22 +0530

    springboot-spring MVC-LoginForm

---
+++ springboot-MVC-LoginForm/src/main/java/com/springboot/example/SpringbootMvcLoginFormApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMvcLoginFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMvcLoginFormApplication.class, args);
+	}
+
+}
